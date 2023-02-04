05e08d1f3580961cd919137ffef0f66cddb2dec6
anaik1234 <101251573+anaik1234@users.noreply.github.com>
2023-02-04 12:21:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, url_for, redirect, render_template
+import wikipedia
+
+class SearchEngineWiki():
+    def Search(self, query):
+        q = wikipedia.summary(query, 10)
+        return q
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    if request.method == "POST":
+        query = request.form.get('Text')
+        query = str(query)
+
+        return redirect(url_for('Search', q=query))
+
+    return render_template('search.html')
+
+@app.route('/<q>', methods=["POST", "GET"])
+def Search(q):
+    if request.method == "POST":
+        query = request.form.get('Text')
+        query = str(query)
+
+        return redirect(url_for('Search', q=query))
+
+        
+    data = SearchEngineWiki().Search(q)
+    return render_template('results.html', d=data, q=q)
+
+if __name__ == "__main__":
+    app.run(debug=True)
