3ee674563a39dd6471f109a21ea69fb43074203b
anaik1234 <101251573+anaik1234@users.noreply.github.com>
2023-03-10 16:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import wikipedia
 
 class SearchEngineWiki():
     def Search(self, query):
-        q = wikipedia.summary(query, 10)
+        try:
+            q = wikipedia.summary(query, 10)
+        except:
+            q = f"Not Found {query} in database"
+
         return q
 
 app = Flask(__name__)
@@ -26,10 +30,14 @@ def Search(q):
 
         return redirect(url_for('Search', q=query))
 
-        
-    data = SearchEngineWiki().Search(q)
-    return render_template('results.html', d=data, q=q)
+    if q.lower() != 'google':
+        data = SearchEngineWiki().Search(q)
+        return render_template('results.html', d=data, q=q)
+    
+    else:
+        g= '''Google LLC is an American multinational technology company focusing on search engine technology, online advertising, cloud computing, computer software, quantum computing, e-commerce, artificial intelligence,[9] and consumer electronics. It has been referred to as "the most powerful company in the world"[10] and one of the world's most valuable brands due to its market dominance, data collection, and technological advantages in the area of artificial intelligence.[11][12][13] Its parent company Alphabet is considered one of the Big Five American information technology companies, alongside Amazon, Apple, Meta, and Microsoft.'''
+        return render_template('results.html', d=g, q=q)
 
 if __name__ == "__main__":
-    app.run(debug=True)
- 
+    app.run(host="192.168.1.22", port=80
+            )
